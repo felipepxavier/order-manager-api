48127d1da2fbaf15243dfc72b76cf12e511ba05b
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2024-06-26 20:50:53 -0300

    feat: add database

---
+++ src/database/config.ts
@@ -0,0 +1,24 @@
+import type { Knex } from 'knex';
+
+export const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'postgres',
+      database: 'ifood_db',
+      password: '123456',
+    },
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'postgres',
+      database: 'ifood_db',
+      password: '123456',
+    },
+  },
+};
