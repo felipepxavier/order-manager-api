6ab5a39989e88a1a49e1093fd472f4b78f21c28e
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2024-12-17 12:35:13 -0300

    refactor: ensure change immediately

--- rds-iac/main.tf
+++ rds-iac/main.tf
@@ -41,6 +41,7 @@ resource "aws_security_group" "rds_sg" {
   name   = "rds_security_group"
   vpc_id = data.aws_vpc.selected_vpc.id
 
+
   ingress {
     from_port       = 5432
     to_port         = 5432
@@ -71,6 +72,7 @@ resource "aws_db_instance" "rds_instance" {
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.rds_private_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id] # Garantir que o RDS usa o security group correto
+  apply_immediately      = true
   skip_final_snapshot    = true
 
   tags = {
